2a1094da20ea398c025f5b0163f413125184189c
seungmyung <choi_4349@naver.com>
2022-12-12 16:29:00 +0900

    최승명첫ì²커밋

---
+++ src/main/java/dev/babsang/megabox/controllers/MovieController.java
@@ -0,0 +1,19 @@
+package dev.babsang.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller(value = "dev.babsang.controllers.MovieController")
+@RequestMapping(value = "movie")
+public class MovieController {
+    @RequestMapping(value = "movie",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getMoviePage() {
+        ModelAndView modelAndView = new ModelAndView("movie/movie");
+        return modelAndView;
+    }
+}
