67ea29f37fc42ae96d24342c17088a0042a077bb
myung <choi_4349@naver.com>
2022-12-23 13:35:06 +0900

    12.23 승명 커밋 1차

---
+++ src/main/java/dev/babsang/megabox/entities/movie/MovieEntity.java
@@ -0,0 +1,164 @@
+package dev.babsang.megabox.entities.movie;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class MovieEntity {
+    private int index;
+    private String title;
+    private String titleEn;
+    private Date releaseDate;
+    private Date endDate;
+    private String summary;
+    private String ageLimit;
+    private String movieState;
+    private String genre;
+    private String director;
+    private String actor;
+    private int runningTime;
+    private int totalAudience;
+    private String screenType;
+    private String moviePoster;
+    private String backgroundImage;
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    public void setBackgroundImage(String backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitleEn() {
+        return titleEn;
+    }
+
+    public void setTitleEn(String titleEn) {
+        this.titleEn = titleEn;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(String ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public String getMovieState() {
+        return movieState;
+    }
+
+    public void setMovieState(String movieState) {
+        this.movieState = movieState;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public int getRunningTime() {
+        return runningTime;
+    }
+
+    public void setRunningTime(int runningTime) {
+        this.runningTime = runningTime;
+    }
+
+    public int getTotalAudience() {
+        return totalAudience;
+    }
+
+    public void setTotalAudience(int totalAudience) {
+        this.totalAudience = totalAudience;
+    }
+
+    public String getScreenType() {
+        return screenType;
+    }
+
+    public void setScreenType(String screenType) {
+        this.screenType = screenType;
+    }
+
+    public String getMoviePoster() {
+        return moviePoster;
+    }
+
+    public void setMoviePoster(String moviePoster) {
+        this.moviePoster = moviePoster;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MovieEntity that = (MovieEntity) o;
+        return index == that.index;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index);
+    }
+}
--- src/main/java/dev/babsang/megabox/mappers/IMovieMapper.java
+++ src/main/java/dev/babsang/megabox/mappers/IMovieMapper.java
@@ -2,15 +2,18 @@ package dev.babsang.megabox.mappers;
 
 import dev.babsang.megabox.entities.member.UserEntity;
 import dev.babsang.megabox.entities.movie.MovieCommentEntity;
+import dev.babsang.megabox.entities.movie.MovieEntity;
+import dev.babsang.megabox.vos.movie.MovieCommentVo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface IMovieMapper {
-    MovieCommentEntity selectMovieCommentByMid(@Param(value = "mid") int mid);
 
+    MovieCommentVo selectMovieCommentByMid(@Param(value = "mid") int mid);
     //유저 나중에 추가하기(로그인 여부 확인하기 위해)
     MovieCommentEntity[] selectMovieCommentsByMid(@Param(value = "mid") int mid);
+    MovieEntity selectMovieByIndex(@Param(value = "mid") int mid);
 
     int insertComment(MovieCommentEntity comment);
 
