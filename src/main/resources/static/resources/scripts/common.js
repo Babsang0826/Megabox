5b55bddbe11b4a949c9dac0e6e8de4697d0a3d35
myung <choi_4349@naver.com>
2023-01-25 12:03:17 +0900

    01.25 승명 커밋 12:03

--- src/main/resources/static/resources/scripts/common.js
+++ src/main/resources/static/resources/scripts/common.js
@@ -149,6 +149,27 @@ for (let i = 0; i < moviePosterTopFour.length; i++) {
     })
 }
 
+const rankPosterBox = document.querySelectorAll('.rank-poster-box');
+const detailMpRank = document.querySelectorAll('.detail-mp-rank');
+const hashtag = document.querySelectorAll('.hashtag');
+const bestImage = document.querySelectorAll('.best-mp-image');
+
+for (let i = 0; i < rankPosterBox.length; i++) {
+    rankPosterBox[i].addEventListener('click', () => {
+        for (let j = 0; j < detailMpRank.length; j++) {
+            detailMpRank[j].style.display = 'none'
+            hashtag[j].style.display = 'none';
+            bestImage[j].style.display = 'none';
+            // rankPosterBox[j].styles.display = 'block'
+        }
+        detailMpRank[i].style.display = 'block';
+        hashtag[i].style.display = 'block';
+        bestImage[i].style.display = 'block';
+        // rankPosterBox[i].style.display = 'none';
+
+        // detailMpRank[i].classList.add('on');
+    })
+}
 
 
 
--- src/main/resources/static/resources/scripts/index.js
+++ src/main/resources/static/resources/scripts/index.js
@@ -14,4 +14,24 @@ const searchMovieBtn = document.querySelector('[rel="searchMovieBtn"]');
 searchMovieBtn.addEventListener('click', () => {
     window.location.href = `/movie/movie?keyword=` + `${searchMovieInput.value}`
 });
-//
+
+
+const rankPosterBox = document.querySelectorAll('.rank-poster-box');
+const detailMpRank = document.querySelectorAll('.detail-mp-rank');
+
+for (let i = 0; i < rankPosterBox.length; i++) {
+    rankPosterBox[i].addEventListener('click', () => {
+
+        detailMpRank[i].classList.add('on');
+    })
+}
+
+
+
+
+
+
+
+
+
+
